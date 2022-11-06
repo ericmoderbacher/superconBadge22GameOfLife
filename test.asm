777e0c50376734933a0e21fef7776db18bd32175
Eric Moderbacher <eric@moderbacher.com>
2022-11-05 20:47:45 -0700

    a non working check point

--- test.asm
+++ test.asm
@@ -1,21 +1,3 @@
-
-;r3 and r4 are the current nibble we are working on
-;r5 is the current bit in that nibble
-
-;r6 and r7 are the nibble we are ping ponging into (either 2 pages forward or 2 back)
-
-;oops i ran out of registers lets try again
-;r1 and r2 are the current nibble we are working on
-;r3 is the current bit in that nibble
-
-;r4 and r5 are the nibble we are ping ponging into (either 2 pages forward or 2 back)
-
-;r6 is the neighbor id we want to check at any given moument
-
-;r7 and r8 are where we will put the calculated nibble address we will find the current neighbor at
-;r9 is where we will put the calculated bit address in the nibble pointed to by r7 and r8
-
-;farts i ran out of registers so how about i do it worse!
 ;r1 and r2 are the current nibble we are working on
 ;r3 is the current bit in that nibble
 
@@ -25,9 +7,7 @@
 ;r7 is where we will put the calculated bit address in the nibble pointed to by r5 and r6
 
 ;r8 is the current state of the current pixel
-;r9 is the accumulated neighbor score
-
-
+;r9 is the accumulated neighbor score (and before that the living or dead status)
 
 ;Need to set the glider on page 2
 MOV R4,0b0100 ;glider line 1
@@ -50,20 +30,30 @@ MOV [0xF0],R0   ;esm the bracket seems to be a literal
 MOV R0,6
 MOV [0xF1],R0
 
-; R3 and R4 are used to keep track of the outermost loop
+; R1 and R2 are used to keep track of the outermost loop
 MOV R1, 0b0001 ;first half of the number 16 00010000
 MOV R2, 0b0000 ;second half
+MOV R3, 0 ;the current bit in that nibble
 
 main:
-;MOV R0,[0xF4] ;hopefullyreads from V flag
 MOV R1, 0b0001 ;first half of the number 16 00010000
 MOV R2, 0b0000 ;second half
-mainSkipingSetup:
-MOV R0,[R1:R2] ;moves the value at 8bit address to R0
-BTG R0, 0 ;toggles the 1th bit
-BTG R0, 1 ;toggles the 4th bit
-BTG R0, 2 ;toggles the 4th bit
-BTG R0, 3 ;toggles the 4th bit
+mainSkipingSetup: ;outer loop (the current nibble of the matrix) begin
+MOV R0, [R1:R2] ;moves the value at 8bit address to R0
+;BTG R0, 0 ;toggles the 1th bit
+;BTG R0, 1 ;toggles the 4th bit
+;BTG R0, 2 ;toggles the 4th bit
+;BTG R0, 3 ;toggles the 4th bit
+bitLoop:
+GOTO theCellFunction
+theCellFunctionReturnPoint:
+inc R3
+MOV R0,R3
+CP R0, 0b0101 ; is R3 == 5?
+SKIP Z,1
+GOTO bitLoop ;done?
+MOV R3, 0 ;all of the bits in the current nibble have been worked so we can reset
+;at this point we have worked all the cell in this nibble
 
 ;BIT RO, 3 ;check new state of that bit sets v flag
 MOV [R1:R2],R0
@@ -74,4 +64,69 @@ MOV R0, R1
 BIT R0,1
 SKIP NZ, 1
 MOV R1, 0b0001
-GOTO mainSkipingSetup
+GOTO mainSkipingSetup ;outer loop (the current nibble of the matrix) end
+
+;The function is the point in our nested loops where we know the nibble and the bit we want to work on
+theCellFunction:
+MOV R9,0
+;at this point the accumulation nibble is not being used
+MOV R0,R3 ;move the current bit count to r0
+CP R0, 0 ;check if current bit in nibble is 0
+SKIP NZ,0 ;if not move on 4 steps ahead
+MOV R0, [R1:R2]
+BIT R0,0
+SKIP Z,1
+MOV R9,1 ;only hit by skip for lines up! ugh!
+
+
+MOV R0,R3 ;move the current bit count to r0
+CP R0, 1 ;check if current bit in nibble is 0
+SKIP NZ,0 ;if not move on 4 steps ahead
+MOV R0, [R1:R2]
+BIT R0,0
+SKIP Z,1
+MOV R9,1 ;only hit by skip for lines up! ugh!
+
+MOV R0,R3 ;move the current bit count to r0
+CP R0, 2 ;check if current bit in nibble is 0
+SKIP NZ,0 ;if not move on 4 steps ahead
+MOV R0, [R1:R2]
+BIT R0,0
+SKIP Z,1
+MOV R9,1 ;only hit by skip for lines up! ugh!
+
+MOV R0,R3 ;move the current bit count to r0
+CP R0, 3 ;check if current bit in nibble is 0
+SKIP NZ,0 ;if not move on 4 steps ahead
+MOV R0, [R1:R2]
+BIT R0,0
+SKIP Z,1
+MOV R9,1 ;only hit by skip for lines up! ugh!
+
+;so at this point we should know if we are alive or not!
+MOV R0,R9
+CP R0,0
+SKIP Z,1
+GOTO deadcellLogic
+GOTO livingCellLogic
+
+deadCellLogicReturnPoint:
+livingCellLogicReturnPoint:
+GOTO theCellFunctionReturnPoint
+
+
+deadCellLogic:
+MOV R0, [R1:R2] ;moves the value at 8bit address to R0
+BTG R0, 0 ;toggles the 1th bit
+MOV [R1:R2], R0
+
+
+
+goto deadCellLogicReturnPoint
+
+livingCellLogic:
+MOV R0, [R1:R2] ;moves the value at 8bit address to R0
+BTG R0, 0 ;toggles the 1th bit
+MOV [R1:R2], R0
+
+goto livingCellLogicReturnPoint
